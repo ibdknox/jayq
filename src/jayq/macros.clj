6c8ca7d4f5b7766e436c16770c51614bd9a9ddfc
Max Penet <m@qbits.cc>
2012-11-24 11:08:12 +0100

    add :let support in do->

--- src/jayq/macros.clj
+++ src/jayq/macros.clj
@@ -13,14 +13,18 @@
                                ~@body)))
 
 (defmacro do->
-  "TODO: add support for :let [] step"
   [m-specs steps & body]
   (let [steps-pairs (partition 2 steps)
         bind (gensym)
         return (gensym)]
     `(let [~bind (:bind ~m-specs)
            ~return (:return ~m-specs)]
-       ~(reduce (fn [m [x f]] `(~bind ~f (fn [~x] ~m)))
+       ~(reduce (fn [m [x f]]
+                  (case x
+                    :let `(let ~f ~m)
+                    `(~bind ~f (fn [~x] ~m))
+                    )
+                  )
                 `(~return (do ~@body))
                 (reverse steps-pairs)))))
 
