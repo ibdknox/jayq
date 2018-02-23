a31983e7600fad73ebdaacabdcc3817a7428dac8
Frank Hale <frankhale@gmail.com>
2018-02-23 13:46:19 -0500

    Update version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jayq "2.5.4"
+(defproject jayq "2.5.5"
   :description "A ClojureScript wrapper for jQuery"
   :license {:name "Eclipse Public License 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
